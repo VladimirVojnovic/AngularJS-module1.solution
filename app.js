f343cce755c94547cb1a4b2798ff4454f44b1b02
Vladimir Vojnović <vladavoj@gmail.com>
2016-09-12 11:43:15 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ function LaunchCheckController($scope, $injector){
   $scope.Checker = function () {
     var stringDishes = $scope.Dishes;
     str.trim(stringDishes);
-
+    var message = "";
     if (stringDishes="") {message = "Please enter data first"}
-    else {var MenuItems = stringDishes.split(",");}
+    else {var MenuItems = stringDishes.split(",");};
 
 
     if (MenuItems.length > 3) {message = "Too much!"}
-    else {message = "Enjoy!"}
+    else {message = "Enjoy!"};
 
   };
     console.log($injector.annotate(LaunchCheckController));
-}
+};
 
 })();
