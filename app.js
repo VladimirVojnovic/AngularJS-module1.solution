4ee114c15ad8bc5c081e6e124c34cb674fe87a71
Vladimir Vojnović <vladavoj@gmail.com>
2016-09-13 11:40:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ LaunchCheckController.$inject = ['$scope'];
 function LaunchCheckController($scope){
   $scope.LaunchMessage = "";
   $scope.Dishes = "";
-  $scope.MenuItems = [];
-  $scope.Checker = function ($scope.Dishes) {
+  var $scope.MenuItems = [];
+  $scope.Checker = function (Dishes) {
     var stringDishes = $scope.Dishes.trim();
     if (stringDishes="") {$scope.LaunchMessage = "Please enter data first"}
-    else {var $scope.MenuItems = stringDishes.split(",")};
+    else {$scope.MenuItems = stringDishes.split(",")};
     if (MenuItems.length > 3) {$scope.LaunchMessage = "Too much!"}
     else {$scope.LaunchMessage = "Enjoy!"};
 
