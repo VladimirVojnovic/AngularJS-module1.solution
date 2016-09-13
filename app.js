beb5d30986dea1d3cc342b7c560d0fc6e23eac90
Vladimir Vojnović <vladavoj@gmail.com>
2016-09-13 09:48:33 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,23 +2,23 @@
 'use strict';
 
 angular.module('LaunchCheck',[])
-.controller('LaunchCheckController', LCController($scope));
+.controller('LaunchCheckController', LaunchCheckController);
 
 LaunchCheckController.$inject = ['$scope'];
-function LCController($scope){
-  var LaunchMessage = "";
-  console.log($scope.Dishes);
-    var stringDishes = $scope.Dishes;
-    str.trim(stringDishes);
+function LaunchCheckController($scope){
+  $scope.LaunchMessage = "";
+  $scope.Dishes = "";
+  $scope.Checker = function () {
+    var stringDishes = $scope.Dishes.trim();
+    if (stringDishes="") {$sope.LaunchMessage = "Please enter data first"}
+    else {var MenuItems = stringDishes.split(",")};
+    if (MenuItems.length > 3) {$scope.LaunchMessage = "Too much!"}
+    else {$scope.LaunchMessage = "Enjoy!"};
 
-    if (stringDishes="") {LaunchMessage = "Please enter data first"}
-    else {var MenuItems = stringDishes.split(",");};
+  };
 
-
-    if (MenuItems.length > 3) {LaunchMessage = "Too much!"}
-    else {LaunchMessage = "Enjoy!"};
 
   //console.log($injector.annotate(LaunchCheckController));
-  }
+};
 
 })();
