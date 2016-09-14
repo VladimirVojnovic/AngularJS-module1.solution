98dfb9b8ef44e294816700445d8a703b7cf942e8
Vladimir Vojnović <vladavoj@gmail.com>
2016-09-14 12:16:22 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ LaunchCheckController.$inject = ['$scope'];
 function LaunchCheckController($scope){
   $scope.LaunchMessage = "";
   $scope.Dishes = "";
-  var MenuItems = [];
   $scope.Checker = function (Dishes) {
     var stringDishes = Dishes.trim();
-    MenuItems = stringDishes.split(',');
-    MenuItems.removeAll(Arrays.asList("", null));
+    var MenuItems = stringDishes.split(',');
+      stringDishes.removeAll(Arrays.asList("", null));
     if (MenuItems.length > 3) {$scope.LaunchMessage = "Too much!"}
         else {$scope.LaunchMessage = "Enjoy!"};
     if (Dishes == "") {$scope.LaunchMessage = "Please enter the data first!"}
