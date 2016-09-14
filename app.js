92515e005c5ad45468b1d656c61dc59590d5dcb3
Vladimir Vojnović <vladavoj@gmail.com>
2016-09-14 09:43:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ LunchCheckController.$inject = ['$scope'];
    $scope.listOfFood = "";
    $scope.checkLunchResult = "";
    $scope.checkLunch = function(list){
-     list = list.split(',').filter(str => /\S/.test(str));/*triend arrow function*/
+     list = list.split(',');
+     list.filter(str => /\S/.test(str));/*triend arrow function*/
      //list = list.split(',').filter(function(str){return /\S/.test(str)} );
      if(list.length > 3){
        $scope.checkLunchResult = "Too much!"
